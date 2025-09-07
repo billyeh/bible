17bacde34455e6d4208248299b2a8004eaaad5a1
Bill Yeh <billyeh@macbookair.lan>
2025-09-06 17:08:56 -0700

    Fresh start

--- lib/main.dart
+++ lib/main.dart
@@ -1,215 +1,122 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import 'bible.dart';
-import 'datePicker.dart';
-import 'nextButton.dart';
-
-const String BOOKS_SELECTED = "BOOKS_SELECTED";
-const String DATE_SELECTED = "DATE_SELECTED";
-
-const String BOOK_NAME_KEY = 'n';
 
 void main() {
-  debugPaintSizeEnabled = true;
-  runApp(new MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Bible Challenge',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => new HomePage(),
-        '/selectBook': (context) => new SelectBookPage(),
-        '/selectDate': (context) => new SelectDatePage(),
-      }
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Text('Hello'),
-            ),
-            Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: NextButton(
-                text: 'Create new challenge!',
-                next: '/selectBook',
-              ),
-            )
-          ]
-        )
-      )
-    );
-  }
-}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-class SelectBookPage extends StatefulWidget {
-  @override
-  _SelectBookState createState() => new _SelectBookState();
-}
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
-class _SelectBookState extends State<SelectBookPage> {
-  List<String> bookNames = new List();
-  HashSet<String> bookChecked = new HashSet<String>();
-  DatabaseHelper db = new DatabaseHelper();
-
-  @override
-  void initState() {
-    super.initState();
-    db.fetchBooks().then((books) {
-      setState(() {
-        books.forEach((book) {
-          bookNames.add(book[BOOK_NAME_KEY]);
-        });
-      });
-    });
-    SharedPreferences.getInstance().then((prefs) {
-      setState(() {
-        if (prefs.getStringList(BOOKS_SELECTED) != null) {
-          bookChecked.addAll(prefs.getStringList(BOOKS_SELECTED));
-        }
-      });
-    });
-  }
-
-  void toggleBook(String book) {
-    setState(() {
-      if (bookChecked.contains(book)) {
-        bookChecked.remove(book);
-      } else {
-        bookChecked.add(book);
-      }
-      SharedPreferences.getInstance().then((prefs) {
-        prefs.setStringList(BOOKS_SELECTED, bookChecked.toList());
-      });
-    });
-  }
-
-  Widget bookListItem(BuildContext context, int index) {
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: () {
-        print('Row tapped');
-        toggleBook(bookNames[index]);
-      },
-      child: Row(
-        children: <Widget>[
-          Checkbox(
-            value: bookChecked.contains(bookNames[index]),
-          ),
-          Text('${bookNames[index]}'),
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Choose which books to read'),
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-                itemCount: bookNames.length,
-                padding: const EdgeInsets.all(5.0),
-                itemBuilder: bookListItem,
-            ),
-          ),
-          Align(
-            alignment: FractionalOffset.bottomCenter,
-            child: NextButton(
-              next: '/selectDate'
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class SelectDatePage extends StatefulWidget {
-  SelectDatePage({Key key}) : super(key: key);
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
+
+  final String title;
+
   @override
-  _SelectDateState createState() => new _SelectDateState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _SelectDateState extends State<SelectDatePage> {
-  DateTime _finishDate = DateTime.now();
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  void getFinishDate() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String date = prefs.getString(DATE_SELECTED);
-    DateTime finishDate = DateTime.now();
-    if (date != '') {
-      finishDate = DateTime.parse(date);
-    }
+  void _incrementCounter() {
     setState(() {
-      _finishDate = finishDate;
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    getFinishDate();
-    return new Scaffold(
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Schedule finish date'),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisSize: MainAxisSize.max,
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Expanded(
-              child: Center(
-                child: DropdownButtonHideUnderline(
-                  child: DatePicker(
-                  labelText: '',
-                    selectedDate: _finishDate,
-                    selectDate: (DateTime date) {
-                      SharedPreferences.getInstance().then((prefs) {
-                        prefs.setString(DATE_SELECTED, date.toString());
-                        getFinishDate();
-                      });
-                    },
-                  ),
-                ),
-              )
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-            Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: NextButton(
-                text: 'Finish',
-                next: '/',
-              ),
-            )
-          ]
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
